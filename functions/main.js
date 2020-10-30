5b835b9482a497b039fd2687d8f158d42117579c
Vitaliy Kozak <>
2020-10-30 11:44:03 +0200

    homework-1

---
+++ functions/main.js
@@ -0,0 +1,46 @@
+document.querySelectorAll('input[name], #formula, #condition').forEach(function ( input)  {
+
+	input.addEventListener('keyup', function () {
+
+		var a1 = document.querySelector('[name="a1"]');
+		var b1 = document.querySelector('[name="b1"]');
+		var a2 = document.querySelector('[name="a2"]');
+		var b2 = document.querySelector('[name="b2"]');
+		var formula = document.querySelector('#formula');
+		var condition = document.querySelector('#condition');
+		var result = document.querySelector('#result');
+
+		try {
+			var calculator = new Function('a1, b1, a2, b2', 'return ' + formula.value + ';');
+			result.value = calculator(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+		} catch (error) {
+			result.value = '#ERROR';
+			console.error(error.message);
+		}
+
+		var demo = new Function('a1, b1, a2, b2', 'return ' + condition.value + ';');
+		var conditionResult = demo(Number(a1.value), Number(b1.value), Number(a2.value), Number(b2.value));
+
+
+		if (conditionResult ) {
+			result.style.background = "green";
+		} else {
+			result.style.background = "transparent";
+		}
+
+	 });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
